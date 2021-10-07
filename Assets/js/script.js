39bf8e7f591e8029311a447373717265950d1788
Nelson Rivera <riveranelson99@gmail.com>
2021-10-06 19:04:07 -0500

    Provided some comments.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,9 @@
+// Establish method here that loads current days date upon page load
+// Date format should follow that of the example image provided
 var $today = moment().format("dddd, MMMM Do");
 $("#currentDay").text($today);
 
+// Establish function that checks for current time (specifically in hours) and color codes each sections accordingly
 function thisMoment() {
     var $currentMoment = moment().format("H");
 
@@ -16,6 +19,7 @@ function thisMoment() {
     })
 }
 
+// Establish function that allows user to save their marked down plan for whatever specific time slot
 function $savePlan (event) {
     var $plan = $(event.target).siblings(".description").val();
     var $time = $(event.target).siblings(".description").attr("id");
@@ -23,6 +27,7 @@ function $savePlan (event) {
     localStorage.setItem($time, $plan);
 }
 
+// Establish function that simply checks if there is any data in local storage and retrieves said data to display for the user
 function storageCheck() {
     $("#9").val(localStorage.getItem("9"));
     $("#10").val(localStorage.getItem("10"));
@@ -35,6 +40,8 @@ function storageCheck() {
     $("#17").val(localStorage.getItem("17"));
 }
 
+// Set the functions that check for current hour and local storage to launch on page load to ensure that the data is displayed correctly according to user desires
+// Add event listener to save button that actually saves any markdowns made
 thisMoment();
 storageCheck();
 $(".saveBtn").on("click", $savePlan);
